ed1360987d492a2dc435c1c0b518569cb3b84973
Peperman66 <peperman66@seznam.cz>
2020-03-10 21:27:50 +0100

    add command handler
    Added command handler

---
+++ index.js
@@ -0,0 +1,23 @@
+const Discord = require('discord.js');
+const fs = require('fs');
+const path = require('path')
+
+const client = new discord.Client();
+const config = require('./config.json');
+client.config = config;
+
+client.commands = new Discord.Collection();
+
+//loading command handler
+fs.readdir(client.config.commandDir, (err, files) => {
+    if (err) return console.log(err);
+
+    let jsFiles = files.filter(f => f.split('.').pop() === 'js');
+    if (jsFiles.length <= 0) return console.log("Could not find any commands!");
+
+    jsFiles.forEach(file => {
+        let props = require(path.join(client.config.commandDir, file));
+        let commandName = props.info.name;
+        client.commands.set(commandName, props);
+    });
+});

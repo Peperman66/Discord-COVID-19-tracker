7482b0a1dbe07273db2abef7d6b47b11eab93efa
Peperman66 <peperman66@seznam.cz>
2020-03-10 22:40:11 +0100

    fix prefix handling
    Fixed prefix handling

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ fs.readdir(client.config.commandDir, (err, files) => {
     if (jsFiles.length <= 0) return console.log("Could not find any commands!");
 
     jsFiles.forEach(file => {
-        let props = require(path.join(client.config.commandDir, file));
+        let props = require(path.join(__dirname, client.config.commandDir, file));
         let commandName = props.info.name;
         client.commands.set(commandName, props);
         console.log(`${file} loaded`);
@@ -34,11 +34,13 @@ client.on('message', async message => {
     if (message.author.bot) return;
     if (message.channel.type === 'dm') return;
 
+    if (!message.content.startsWith(client.config.prefix)) return;
+
     let prefix = client.config.prefix;
     let messageArray = message.content.split(' ');
-    let cmd = messageArray[0];
+    let cmd = messageArray[1];
     let args = messageArray.splice(1);
-    let commandFile = client.commands.get(cmd.slice(prefix.length));
+    let commandFile = client.commands.get(cmd);
     if (!commandFile) return;
 
     if (cmd.slice(0, client.config.prefix.length) === client.config.prefix) {

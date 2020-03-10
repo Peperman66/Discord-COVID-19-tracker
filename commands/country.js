e4c43bc9a34ec3519851a77295556fafe63488f1
Peperman66 <peperman66@seznam.cz>
2020-03-10 23:11:45 +0100

    add logging messages to country command
    Added logging messages to country command

--- commands/country.js
+++ commands/country.js
@@ -4,16 +4,26 @@ const https = require('https');
 module.exports.run = async (client, message, args) => {
     if (args.length != 1) return message.reply(`Usage: ${client.config.prefix}country <country code>`);
 
+    let tempMessage = await message.channel.send('Requesting data...');
+
     https.get('https://corona.lmao.ninja/countries', {}, res => {
         let data = '';
+        let dataRetrieving = false
         res.on('data', chunk => {
             data += chunk;
+            if (!dataRetrieving) {
+                tempMessage.edit('Retrieving data...')
+                .then(newMessage => tempMessage = newMessage);
+                dataRetrieving = true;
+            }
         });
 
         res.on('end', () => {
+            tempMessage.edit('Data retrieved!')
+                .then(newMessage => newMessage.delete({ timeout: 1000 }));
             let dataJSON = JSON.parse(data);
             let dataCountry = dataJSON.filter(obj => {
-                return obj.country === args[0]
+                return obj.country === args[0];
             });
             if (!dataCountry) return message.reply(`Country code is invalid. For list of country codes, use ${client.config.prefix}countrycodes`);
 
@@ -28,6 +38,11 @@ module.exports.run = async (client, message, args) => {
                 .addField('Today\'s deaths', dataCountry[0].todayDeaths, true);
             message.channel.send(embed);
         });
+
+        res.on('error', (err) => {
+            console.log(err);
+            tempMessage.edit('Something went wrong. Please try again in a few minutes.');
+        })
     });
 }
 

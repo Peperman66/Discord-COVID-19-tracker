94b192492bcc19c87fc0d2d0e5769c10072bceeb
Peperman66 <peperman66@seznam.cz>
2020-03-10 22:03:30 +0100

    add message event
    Added message event

--- index.js
+++ index.js
@@ -28,6 +28,21 @@ client.on('ready', () => {
     client.user.setActivity('the coronavirus outbreak | ,corona help for help', {type: 'WATCHING'});
 });
 
+client.on('message', async message => {
+    if (message.author.bot) return;
+    if (message.channel.type === 'dm') return;
 
+    let prefix = client.config.prefix;
+    let messageArray = message.content.split(' ');
+    let cmd = messageArray[0];
+    let args = messageArray.splice(1);
+    let commandFile = client.commands.get(cmd.slice(prefix.length));
+    if (!commandFile) return;
+
+    if (cmd.slice(0, client.config.prefix.length) === client.config.prefix) {
+        await commandFile.run(client, message, args)
+            .catch((err) => console.log(err));
+    }
+})
 
 client.login(config.token)

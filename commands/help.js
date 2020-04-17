47f9f2561cf40d26d0b8843cf20e210d3de1ae3c
Peperman66 <peperman66@seznam.cz>
2020-04-17 16:11:04 +0200

    add continent command to list of commands
    Added continent command to list of commands

--- commands/help.js
+++ commands/help.js
@@ -3,8 +3,9 @@ const Discord = require('discord.js');
 module.exports.run = async (client, message, args) => {
     
     let helpMessage = "**Available commands:**\n" +
-    `\`\`${client.config.prefix}country <countrycode>\`\`: Displays current COVID-19 information in specified country\n` +
-    `\`\`${client.config.prefix}world\`\`: Displays current COVID-19 information in the world\n` + 
+    `\`\`${client.config.prefix}country <country>\`\`: Displays current COVID-19 information in the specified country\n` +
+    `\`\`${client.config.prefix}continent <continent>\`\`: Displays current COVID-19 information in the specified continent\n` +
+    `\`\`${client.config.prefix}world\`\`: Displays current COVID-19 information in the world\n` +
     `\`\`${client.config.prefix}help\`\`: Shows this message`;
 
     message.channel.send(helpMessage);

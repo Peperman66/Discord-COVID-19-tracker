ee107741f568c4855b4802de52de9ffcd1b11294
Peperman66 <peperman66@seznam.cz>
2020-04-17 15:57:31 +0200

    add country parameter to request in country command
    Added country parameter to request in country command

--- commands/country.js
+++ commands/country.js
@@ -3,11 +3,11 @@ const https = require('https');
 
 module.exports.run = async (client, message, args) => {
     if (args.length < 1) return message.reply(`Usage: \`\`${client.config.prefix}country <country code>\`\``);
-    let countryString = args.join(" ");
+    let countryString =encodeURIComponent(args.join(" "));
 
     let tempMessage = await message.channel.send('Requesting data...');
 
-    https.get('https://corona.lmao.ninja/v2/countries', {}, res => {
+    https.get(`https://corona.lmao.ninja/v2/countries/${countryString}`, {}, res => {
         let data = '';
         let dataRetrieving = false
         res.on('data', chunk => {
@@ -30,29 +30,26 @@ module.exports.run = async (client, message, args) => {
                 console.log(err);
                 return;
             }
-            let dataCountry = dataJSON.filter(obj => {
-                return obj.country.toLowerCase() === countryString.toLowerCase();
-            });
-            if (dataCountry.length === 0) return tempMessage.edit(`Country code is invalid. For list of country codes, use \`\`${client.config.prefix}countrycodes\`\``);
-            dataCountry = dataCountry[0]
+            if (res.statusCode == 404) return tempMessage.edit(`Can't find specified country. Maybe that country has no recorded cases?`);
+            if (res.statusCode != 200) return tempMessage.edit('Something went wrong. Please try again later.');
             let embed = new Discord.MessageEmbed()
-                .setTitle(`Coronavirus outbreak in ${dataCountry.country} (${dataCountry.countryInfo.iso3})`)
-                .setThumbnail(dataCountry.countryInfo.flag)
-                .addField('Total cases', dataCountry.cases, true)
-                .addField('Today\'s cases', dataCountry.todayCases, true)
-                .addField('Cases per one million', dataCountry.casesPerOneMillion, true)
-                .addField('Deaths', dataCountry.deaths, true)
-                .addField('Today\'s deaths', dataCountry.todayDeaths, true)
-                .addField('Deaths per one million', dataCountry.deathsPerOneMillion, true)
-                .addField('Tests', dataCountry.tests, true)
+                .setTitle(`Coronavirus outbreak in ${dataJSON.country} (${dataJSON.countryInfo.iso3})`)
+                .setThumbnail(dataJSON.countryInfo.flag)
+                .addField('Total cases', dataJSON.cases, true)
+                .addField('Today\'s cases', dataJSON.todayCases, true)
+                .addField('Cases per one million', dataJSON.casesPerOneMillion, true)
+                .addField('Deaths', dataJSON.deaths, true)
+                .addField('Today\'s deaths', dataJSON.todayDeaths, true)
+                .addField('Deaths per one million', dataJSON.deathsPerOneMillion, true)
+                .addField('Tests', dataJSON.tests, true)
                 .addField('\u200B', '\u200B', true)
-                .addField('Tests per one million', dataCountry.testsPerOneMillion, true)
+                .addField('Tests per one million', dataJSON.testsPerOneMillion, true)
                 .addField('\u200B', '\u200B', false)
-                .addField('Recovered', dataCountry.recovered, true)
-                .addField('Critical', dataCountry.critical, true)
-                .addField('Active', dataCountry.active, true)
+                .addField('Recovered', dataJSON.recovered, true)
+                .addField('Critical', dataJSON.critical, true)
+                .addField('Active', dataJSON.active, true)
                 .setFooter('Last updated')
-                .setTimestamp(dataCountry.updated);
+                .setTimestamp(dataJSON.updated);
             message.channel.send(embed);
             tempMessage.delete({timeout: 5000});
         });
@@ -60,7 +57,7 @@ module.exports.run = async (client, message, args) => {
         res.on('error', (err) => {
             console.log(err);
             tempMessage.edit('Something went wrong. Please try again later.');
-        })
+        });
     });
 }
 

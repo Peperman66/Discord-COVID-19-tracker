2da887c66c49ecf11db9b4b58f82ceafc5d43f65
Peperman66 <peperman66@seznam.cz>
2020-04-17 15:56:55 +0200

    remove countrycodes command from help
    Removed countrycodes command from help command

--- commands/help.js
+++ commands/help.js
@@ -5,7 +5,6 @@ module.exports.run = async (client, message, args) => {
     let helpMessage = "**Available commands:**\n" +
     `\`\`${client.config.prefix}country <countrycode>\`\`: Displays current COVID-19 information in specified country\n` +
     `\`\`${client.config.prefix}world\`\`: Displays current COVID-19 information in the world\n` + 
-    `\`\`${client.config.prefix}countrycodes\`\`: Shows list of all possible countrycodes\n` + 
     `\`\`${client.config.prefix}help\`\`: Shows this message`;
 
     message.channel.send(helpMessage);
